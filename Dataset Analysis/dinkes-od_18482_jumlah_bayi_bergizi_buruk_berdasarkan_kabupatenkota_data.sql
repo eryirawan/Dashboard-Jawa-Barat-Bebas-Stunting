cd822752bd11a85779a4466e95667ba1a4fa22c7
Ery <102465817+eryirawan@users.noreply.github.com>
2022-09-12 14:46:14 +0700

    Add files via upload

---
+++ Dataset Analysis/dinkes-od_18482_jumlah_bayi_bergizi_buruk_berdasarkan_kabupatenkota_data.sql
@@ -0,0 +1,91 @@
+CREATE TABLE mytable(
+   id                      INTEGER  NOT NULL PRIMARY KEY 
+  ,kode_provinsi           INTEGER  NOT NULL
+  ,nama_provinsi           VARCHAR(10) NOT NULL
+  ,kode_kabupaten_kota     INTEGER  NOT NULL
+  ,nama_kabupaten_kota     VARCHAR(23) NOT NULL
+  ,jumlah_ibu_hamil_anemia INTEGER  NOT NULL
+  ,satuan                  VARCHAR(5) NOT NULL
+  ,tahun                   INTEGER  NOT NULL
+);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (1,32,'JAWA BARAT',3201,'KABUPATEN BOGOR',98,'ORANG',2016);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (2,32,'JAWA BARAT',3202,'KABUPATEN SUKABUMI',186,'ORANG',2016);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (3,32,'JAWA BARAT',3203,'KABUPATEN CIANJUR',323,'ORANG',2016);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (4,32,'JAWA BARAT',3204,'KABUPATEN BANDUNG',66,'ORANG',2016);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (5,32,'JAWA BARAT',3205,'KABUPATEN GARUT',143,'ORANG',2016);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (6,32,'JAWA BARAT',3206,'KABUPATEN TASIKMALAYA',63,'ORANG',2016);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (7,32,'JAWA BARAT',3207,'KABUPATEN CIAMIS',120,'ORANG',2016);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (8,32,'JAWA BARAT',3208,'KABUPATEN KUNINGAN',49,'ORANG',2016);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (9,32,'JAWA BARAT',3209,'KABUPATEN CIREBON',182,'ORANG',2016);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (10,32,'JAWA BARAT',3210,'KABUPATEN MAJALENGKA',18,'ORANG',2016);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (11,32,'JAWA BARAT',3211,'KABUPATEN SUMEDANG',44,'ORANG',2016);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (12,32,'JAWA BARAT',3212,'KABUPATEN INDRAMAYU',26,'ORANG',2016);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (13,32,'JAWA BARAT',3213,'KABUPATEN SUBANG',53,'ORANG',2016);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (14,32,'JAWA BARAT',3214,'KABUPATEN PURWAKARTA',100,'ORANG',2016);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (15,32,'JAWA BARAT',3215,'KABUPATEN KARAWANG',281,'ORANG',2016);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (16,32,'JAWA BARAT',3216,'KABUPATEN BEKASI',299,'ORANG',2016);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (17,32,'JAWA BARAT',3217,'KABUPATEN BANDUNG BARAT',37,'ORANG',2016);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (18,32,'JAWA BARAT',3218,'KABUPATEN PANGANDARAN',88,'ORANG',2016);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (19,32,'JAWA BARAT',3271,'KOTA BOGOR',57,'ORANG',2016);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (20,32,'JAWA BARAT',3272,'KOTA SUKABUMI',29,'ORANG',2016);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (21,32,'JAWA BARAT',3273,'KOTA BANDUNG',374,'ORANG',2016);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (22,32,'JAWA BARAT',3274,'KOTA CIREBON',43,'ORANG',2016);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (23,32,'JAWA BARAT',3275,'KOTA BEKASI',142,'ORANG',2016);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (24,32,'JAWA BARAT',3276,'KOTA DEPOK',85,'ORANG',2016);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (25,32,'JAWA BARAT',3277,'KOTA CIMAHI',42,'ORANG',2016);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (26,32,'JAWA BARAT',3278,'KOTA TASIKMALAYA',83,'ORANG',2016);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (27,32,'JAWA BARAT',3279,'KOTA BANJAR',1,'ORANG',2016);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (28,32,'JAWA BARAT',3201,'KABUPATEN BOGOR',3120,'ORANG',2017);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (29,32,'JAWA BARAT',3202,'KABUPATEN SUKABUMI',2546,'ORANG',2017);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (30,32,'JAWA BARAT',3203,'KABUPATEN CIANJUR',1808,'ORANG',2017);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (31,32,'JAWA BARAT',3204,'KABUPATEN BANDUNG',611,'ORANG',2017);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (32,32,'JAWA BARAT',3205,'KABUPATEN GARUT',195,'ORANG',2017);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (33,32,'JAWA BARAT',3206,'KABUPATEN TASIKMALAYA',1200,'ORANG',2017);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (34,32,'JAWA BARAT',3207,'KABUPATEN CIAMIS',212,'ORANG',2017);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (35,32,'JAWA BARAT',3208,'KABUPATEN KUNINGAN',299,'ORANG',2017);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (36,32,'JAWA BARAT',3209,'KABUPATEN CIREBON',1853,'ORANG',2017);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (37,32,'JAWA BARAT',3210,'KABUPATEN MAJALENGKA',333,'ORANG',2017);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (38,32,'JAWA BARAT',3211,'KABUPATEN SUMEDANG',578,'ORANG',2017);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (39,32,'JAWA BARAT',3212,'KABUPATEN INDRAMAYU',585,'ORANG',2017);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (40,32,'JAWA BARAT',3213,'KABUPATEN SUBANG',504,'ORANG',2017);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (41,32,'JAWA BARAT',3214,'KABUPATEN PURWAKARTA',476,'ORANG',2017);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (42,32,'JAWA BARAT',3215,'KABUPATEN KARAWANG',501,'ORANG',2017);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (43,32,'JAWA BARAT',3216,'KABUPATEN BEKASI',294,'ORANG',2017);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (44,32,'JAWA BARAT',3217,'KABUPATEN BANDUNG BARAT',452,'ORANG',2017);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (45,32,'JAWA BARAT',3218,'KABUPATEN PANGANDARAN',69,'ORANG',2017);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (46,32,'JAWA BARAT',3271,'KOTA BOGOR',381,'ORANG',2017);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (47,32,'JAWA BARAT',3272,'KOTA SUKABUMI',115,'ORANG',2017);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (48,32,'JAWA BARAT',3273,'KOTA BANDUNG',224,'ORANG',2017);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (49,32,'JAWA BARAT',3274,'KOTA CIREBON',216,'ORANG',2017);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (50,32,'JAWA BARAT',3275,'KOTA BEKASI',586,'ORANG',2017);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (51,32,'JAWA BARAT',3276,'KOTA DEPOK',389,'ORANG',2017);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (52,32,'JAWA BARAT',3277,'KOTA CIMAHI',193,'ORANG',2017);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (53,32,'JAWA BARAT',3278,'KOTA TASIKMALAYA',463,'ORANG',2017);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (54,32,'JAWA BARAT',3279,'KOTA BANJAR',87,'ORANG',2017);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (55,32,'JAWA BARAT',3201,'KABUPATEN BOGOR',79,'ORANG',2018);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (56,32,'JAWA BARAT',3202,'KABUPATEN SUKABUMI',63,'ORANG',2018);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (57,32,'JAWA BARAT',3203,'KABUPATEN CIANJUR',182,'ORANG',2018);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (58,32,'JAWA BARAT',3204,'KABUPATEN BANDUNG',0,'ORANG',2018);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (59,32,'JAWA BARAT',3205,'KABUPATEN GARUT',78,'ORANG',2018);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (60,32,'JAWA BARAT',3206,'KABUPATEN TASIKMALAYA',30,'ORANG',2018);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (61,32,'JAWA BARAT',3207,'KABUPATEN CIAMIS',106,'ORANG',2018);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (62,32,'JAWA BARAT',3208,'KABUPATEN KUNINGAN',0,'ORANG',2018);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (63,32,'JAWA BARAT',3209,'KABUPATEN CIREBON',306,'ORANG',2018);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (64,32,'JAWA BARAT',3210,'KABUPATEN MAJALENGKA',11,'ORANG',2018);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (65,32,'JAWA BARAT',3211,'KABUPATEN SUMEDANG',51,'ORANG',2018);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (66,32,'JAWA BARAT',3212,'KABUPATEN INDRAMAYU',47,'ORANG',2018);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (67,32,'JAWA BARAT',3213,'KABUPATEN SUBANG',5,'ORANG',2018);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (68,32,'JAWA BARAT',3214,'KABUPATEN PURWAKARTA',72,'ORANG',2018);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (69,32,'JAWA BARAT',3215,'KABUPATEN KARAWANG',0,'ORANG',2018);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (70,32,'JAWA BARAT',3216,'KABUPATEN BEKASI',147,'ORANG',2018);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (71,32,'JAWA BARAT',3217,'KABUPATEN BANDUNG BARAT',17,'ORANG',2018);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (72,32,'JAWA BARAT',3218,'KABUPATEN PANGANDARAN',54,'ORANG',2018);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (73,32,'JAWA BARAT',3271,'KOTA BOGOR',65,'ORANG',2018);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (74,32,'JAWA BARAT',3272,'KOTA SUKABUMI',15,'ORANG',2018);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (75,32,'JAWA BARAT',3273,'KOTA BANDUNG',648,'ORANG',2018);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (76,32,'JAWA BARAT',3274,'KOTA CIREBON',37,'ORANG',2018);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (77,32,'JAWA BARAT',3275,'KOTA BEKASI',209,'ORANG',2018);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (78,32,'JAWA BARAT',3276,'KOTA DEPOK',76,'ORANG',2018);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (79,32,'JAWA BARAT',3277,'KOTA CIMAHI',31,'ORANG',2018);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (80,32,'JAWA BARAT',3278,'KOTA TASIKMALAYA',0,'ORANG',2018);
+INSERT INTO mytable(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,jumlah_ibu_hamil_anemia,satuan,tahun) VALUES (81,32,'JAWA BARAT',3279,'KOTA BANJAR',0,'ORANG',2018);
